d712097ed025e89969b5266ae87811ef09b2af84
Victor <vdiazroa@gmail.com>
2018-10-02 21:49:32 +0200

    added init

--- js/sounds.js
+++ js/sounds.js
@@ -5,14 +5,21 @@ for(let i = 0 ; i < 8 ; i++){
 }
 addFalseValues();
 
+//add False Value to the Array
+function addFalseValues () {
+  for(let i = 0 ; i < 8 ; i++){
+    for(let j = 0 ; j < 8 ; j++){
+    steps[i][j] = false;
+    }
+  }
+}
+
 //Audio API
-let gmin = [195.995, 220, 233.082, 261.626, 293.665, 311.127, 349.228, 391.995];
+const gmin = [195.995, 220, 233.082, 261.626, 293.665, 311.127, 349.228, 391.995];
 
-let sounds = new AudioContext(), tinput = 4000, stop;
+let sounds = new AudioContext(), tinput = 4000, instrument = "sine", stop;
 
-let instrument = "sine";
-
-function play(step, time, tone) {
+const play = (step, time, tone) => {
   let chord = [];
   for (let i = 0; i < 8; i++) {
 
@@ -29,7 +36,7 @@ function play(step, time, tone) {
 }
 
 //add Sound to clicked Pads
-function soundToggle(line, row) {
+const soundToggle = (line, row) => {
   if (steps[row][line]) {
     steps[row][line] = false;
   } 
@@ -39,7 +46,7 @@ function soundToggle(line, row) {
 }
 
 //Play ActionButton
-function soundloop(time) {
+const soundloop = time => {
   if (stop) {
     stop = false;
     return;
@@ -47,39 +54,31 @@ function soundloop(time) {
 
   for (i = 0; i < 8; ++i) {
     let j = i;
-    setTimeout(function () { stepPlayClasses(j); play(steps[j], time / 8000, instrument) }, time * j / 8);
+    setTimeout( () => { stepPlayClasses(j); play(steps[j], time / 8000, instrument) }, time * j / 8);
   }
 
-  setTimeout(function () { stepPlayClasses(8) }, time);
-  setTimeout(function () { soundloop(tinput) }, time);
+  setTimeout( () => { stepPlayClasses(8) }, time);
+  setTimeout( () => { soundloop(tinput) }, time);
 }
 
-//add False Value to the Array
-function addFalseValues () {
-  for(let i = 0 ; i < 8 ; i++){
-    for(let j = 0 ; j < 8 ; j++){
-    steps[i][j] = false;
+const init = () => {
+  //EventListeners
+  document.querySelector("#play").addEventListener("click", () => { soundloop(4000) });
+  document.querySelector("#stop").addEventListener("click", () => { stop = true });
+  document.querySelector("#trash").addEventListener("click", () => { clearSelectedPads(); addFalseValues() });
+  document.querySelector("#myRange").addEventListener("input", (e) => { tinput = 480000 / (e.target.value) });
+  document.querySelectorAll(".pad").forEach( pad => { pad.addEventListener("click", () =>{ clickBehavior(pad) }); });
+
+  //EventListener add sound changing arrayValues
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      document.querySelector("#step-" + (i + 1)).children[j].addEventListener("click", () => { soundToggle(j, i) });
     }
   }
-}
 
-//EventListeners
-document.querySelector("#play").addEventListener("click", function () { soundloop(4000) });
-document.querySelector("#stop").addEventListener("click", function () { stop = true });
-document.querySelector("#trash").addEventListener("click", function () { clearSelectedPads(); addFalseValues() });
-document.querySelector("#myRange").addEventListener("input", function (e) { tinput = 480000 / (e.target.value) });
-document.querySelectorAll(".pad").forEach( pad => { pad.addEventListener("click", () =>{ clickBehavior(pad) }); });
-
-//EventListener add sound changing arrayValues
-for (let i = 0; i < 8; i++) {
-  for (let j = 0; j < 8; j++) {
-    document.querySelector("#step-" + (i + 1)).children[j].addEventListener("click", function () { soundToggle(j, i) });
-  }
-}
-
-//EventListener select instruments changing the tone
-var selectInstrument = document.getElementById("instruments");
-  selectInstrument.addEventListener("change", function() {
+  //EventListener select instruments changing the tone
+  var selectInstrument = document.getElementById("instruments");
+  selectInstrument.addEventListener("change", () => {
     if (selectInstrument.value === "Synth"){
       instrument="sine";
     }
@@ -87,3 +86,4 @@ var selectInstrument = document.getElementById("instruments");
       instrument="sawtooth";
     };
   });
+}
