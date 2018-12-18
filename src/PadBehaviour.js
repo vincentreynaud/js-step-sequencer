70742dbf33b7ae2823e307567d44259fbf418e74
Vincent Reynaud <vincentreynaud.de@gmail.com>
2018-12-18 12:50:37 +0100

    code refractoring

--- src/PadBehaviour.js
+++ src/PadBehaviour.js
@@ -1,36 +1,36 @@
-class Pads {
+"use strict";
+
+class PadBehaviour {
   togglePadPressedClass(pad) {
     pad.classList.contains("pad-pressed")
-    ? pad.classList.remove("pad-pressed")
-    : pad.classList.add("pad-pressed");
+      ? pad.classList.remove("pad-pressed")
+      : pad.classList.add("pad-pressed");
   }
-  
+
   clearSelectedPads() {
     document.querySelectorAll(".pad-pressed").forEach(pad => {
       pad.classList.remove("pad-pressed");
     });
   }
-  
+
   toggleStepPlayClass(step) {
     if (step === 0) {
       this.addStepPlayClass(step);
     }
     else if (step === 8) {
-      this.removeStepPlayClass(step);    
+      this.removeStepPlayClass(step);
     }
     else {
-      this.removeStepPlayClass(step); 
+      this.removeStepPlayClass(step);
       this.addStepPlayClass(step);
-     };
+    };
   }
-  
+
   addStepPlayClass(step) {
-    document.querySelector(`#step-${step+1}`).classList.add("step-play");   
+    document.querySelector(`#step-${step + 1}`).classList.add("step-play");
   }
-  
+
   removeStepPlayClass(step) {
     document.querySelector(`#step-${step}`).classList.remove("step-play");
   }
 }
-
-export default Pads;
